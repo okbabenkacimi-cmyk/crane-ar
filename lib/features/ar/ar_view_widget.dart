788c2210b230b4fa19288529e2ee33a7efa34b66
okbabenkacimi-cmyk <okba.benkacimi@gmail.com>
2026-09-20 22:51:14 +0100

    Create ar_view_widget.dart

---
+++ lib/features/ar/ar_view_widget.dart
@@ -0,0 +1,42 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../core/constants.dart';
+
+/// Hosts the native ARCore GLSurfaceView inside the Flutter tree.
+class ArViewWidget extends StatelessWidget {
+  const ArViewWidget({super.key, this.onPlatformViewCreated});
+
+  final ValueChanged<int>? onPlatformViewCreated;
+
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb || !Platform.isAndroid) {
+      return const ColoredBox(
+        color: Colors.black,
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: Text(
+              'The AR prototype currently targets Android devices with ARCore.\n'
+              'iOS / ARKit support is planned.',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white70),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return AndroidView(
+      viewType: ArChannels.arViewType,
+      creationParams: const <String, dynamic>{},
+      creationParamsCodec: const StandardMessageCodec(),
+      onPlatformViewCreated: onPlatformViewCreated,
+      gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+    );
+  }
+}
