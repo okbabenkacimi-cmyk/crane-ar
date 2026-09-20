1f913d8f93375234e74e1efd94a1e4ba2572ec1a
okbabenkacimi-cmyk <okba.benkacimi@gmail.com>
2026-09-20 23:16:57 +0100

    Update ar_view_widget.dart

--- lib/features/ar/ar_view_widget.dart
+++ lib/features/ar/ar_view_widget.dart
@@ -3,6 +3,7 @@ import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/gestures.dart';
 
 import '../../core/constants.dart';
 
